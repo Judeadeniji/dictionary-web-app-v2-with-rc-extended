cf3e6dc964fb44c132d81713cb77071036a25864
Olatoyan <golatoyan@gmail.com>
2024-01-11 05:03:02 +0100

    added a little animation in changing fonts box

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import FontChangerBox from "./FontChangerBox";
 import Logo from "./Logo";
 import ThemeChangerBox from "./ThemeChangerBox";
 import { useDisplay } from "../contexts/DisplayContext";
+import { AnimatePresence, motion } from "framer-motion";
 
 function Header() {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -22,43 +23,49 @@ function Header() {
         <div className="bg-e9e9e9 h-full w-[0.1rem]"></div>
         <ThemeChangerBox />
 
-        {isNavOpen && (
-          <div
-            className={`absolute -left-[6rem] top-[5rem] flex w-[18rem] flex-col gap-5 rounded-[1.6rem]  p-[2.4rem] text-[1.8rem] leading-[2.4rem] ${
-              isDarkMode
-                ? "shadow-purple-sh bg-[#1f1f1f] text-white"
-                : "shadow-white-sh text-2D2D2D bg-white"
-            }`}
-          >
-            <span
-              className="hover:text-a445ed font-sans-serif cursor-pointer text-[1.8rem] font-bold leading-[2.4rem]"
-              onClick={() => {
-                closeNavBox();
-                setFont("sans-serif");
-              }}
+        <AnimatePresence>
+          {isNavOpen && (
+            <motion.div
+              className={`absolute -left-[6rem] top-[5rem] flex w-[18rem] flex-col gap-5 rounded-[1.6rem]  p-[2.4rem] text-[1.8rem] leading-[2.4rem] ${
+                isDarkMode
+                  ? "shadow-purple-sh bg-[#1f1f1f] text-white"
+                  : "shadow-white-sh text-2D2D2D bg-white"
+              }`}
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: [1, 1.2, 1] }}
+              exit={{ opacity: 0, scale: 0 }}
+              transition={{ duration: 0.5 }}
             >
-              Sans Serif
-            </span>
-            <span
-              className="hover:text-a445ed cursor-pointer font-serif text-[1.8rem] font-bold leading-[2.4rem]"
-              onClick={() => {
-                closeNavBox();
-                setFont("serif");
-              }}
-            >
-              Serif
-            </span>
-            <span
-              className="hover:text-a445ed cursor-pointer font-mono text-[1.8rem] font-bold leading-[2.4rem]"
-              onClick={() => {
-                closeNavBox();
-                setFont("mono");
-              }}
-            >
-              Mono
-            </span>
-          </div>
-        )}
+              <span
+                className="hover:text-a445ed font-sans-serif cursor-pointer text-[1.8rem] font-bold leading-[2.4rem]"
+                onClick={() => {
+                  closeNavBox();
+                  setFont("sans-serif");
+                }}
+              >
+                Sans Serif
+              </span>
+              <span
+                className="hover:text-a445ed cursor-pointer font-serif text-[1.8rem] font-bold leading-[2.4rem]"
+                onClick={() => {
+                  closeNavBox();
+                  setFont("serif");
+                }}
+              >
+                Serif
+              </span>
+              <span
+                className="hover:text-a445ed cursor-pointer font-mono text-[1.8rem] font-bold leading-[2.4rem]"
+                onClick={() => {
+                  closeNavBox();
+                  setFont("mono");
+                }}
+              >
+                Mono
+              </span>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </nav>
     </header>
   );
