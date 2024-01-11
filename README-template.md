fa2b264c12a793016ec70e50dd6bf4e0c2cefde0
Olatoyan <golatoyan@gmail.com>
2024-01-11 15:58:07 +0100

    fixed a little animation

--- README-template.md
+++ README-template.md
@@ -1,6 +1,6 @@
 # Frontend Mentor - Dictionary web app solution
 
-This is a solution to the [Dictionary web app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/dictionary-web-app-h5wwnyuKFL). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+This is a solution to the [Dictionary web app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/dictionary-web-app-h5wwnyuKFL). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
 ## Table of contents
 
@@ -34,21 +34,9 @@ Users should be able to:
 - See hover and focus states for all interactive elements on the page
 - **Bonus**: Have the correct color scheme chosen for them based on their computer preferences. _Hint_: Research `prefers-color-scheme` in CSS.
 
-### Screenshot
-
-![](./screenshot.jpg)
-
-Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
-
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
-
-Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
-
-**Note: Delete this note and the paragraphs above when you add your screenshot. If you prefer not to add a screenshot, feel free to remove this entire section.**
-
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Solution URL: [https://github.com/Olatoyan/dictionary-web-app-v2](https://github.com/Olatoyan/dictionary-web-app-v2)
 - Live Site URL: [Add live site URL here](https://your-live-site-url.com)
 
 ## My process
@@ -75,15 +63,17 @@ To see how you can add code snippets, see below:
 ```html
 <h1>Some HTML code I'm proud of</h1>
 ```
+
 ```css
 .proud-of-this-css {
   color: papayawhip;
 }
 ```
+
 ```js
 const proudOfThisFunc = () => {
-  console.log('🎉')
-}
+  console.log("🎉");
+};
 ```
 
 If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,12 +15,12 @@ function Header() {
   }
 
   return (
-    <header className="mobile:px-0 flex justify-between px-12 py-[5.5rem]">
+    <header className="flex justify-between px-12 py-[5.5rem] mobile:px-0">
       <Logo />
 
       <nav className="relative flex items-center gap-8">
         <FontChangerBox closeNavBox={closeNavBox} />
-        <div className="bg-e9e9e9 h-full w-[0.1rem]"></div>
+        <div className="h-full w-[0.1rem] bg-e9e9e9"></div>
         <ThemeChangerBox />
 
         <AnimatePresence>
@@ -28,16 +28,16 @@ function Header() {
             <motion.div
               className={`absolute -left-[6rem] top-[5rem] flex w-[18rem] flex-col gap-5 rounded-[1.6rem]  p-[2.4rem] text-[1.8rem] leading-[2.4rem] ${
                 isDarkMode
-                  ? "shadow-purple-sh bg-[#1f1f1f] text-white"
-                  : "shadow-white-sh text-2D2D2D bg-white"
+                  ? "bg-[#1f1f1f] text-white shadow-purple-sh"
+                  : "bg-white text-2D2D2D shadow-white-sh"
               }`}
               initial={{ opacity: 0, scale: 0 }}
-              animate={{ opacity: 1, scale: [1, 1.2, 1] }}
+              animate={{ opacity: 1, scale: [0, 1, 1.2, 1] }}
               exit={{ opacity: 0, scale: 0 }}
               transition={{ duration: 0.5 }}
             >
               <span
-                className="hover:text-a445ed font-sans-serif cursor-pointer text-[1.8rem] font-bold leading-[2.4rem]"
+                className="cursor-pointer font-sans-serif text-[1.8rem] font-bold leading-[2.4rem] hover:text-a445ed"
                 onClick={() => {
                   closeNavBox();
                   setFont("sans-serif");
@@ -46,7 +46,7 @@ function Header() {
                 Sans Serif
               </span>
               <span
-                className="hover:text-a445ed cursor-pointer font-serif text-[1.8rem] font-bold leading-[2.4rem]"
+                className="cursor-pointer font-serif text-[1.8rem] font-bold leading-[2.4rem] hover:text-a445ed"
                 onClick={() => {
                   closeNavBox();
                   setFont("serif");
@@ -55,7 +55,7 @@ function Header() {
                 Serif
               </span>
               <span
-                className="hover:text-a445ed cursor-pointer font-mono text-[1.8rem] font-bold leading-[2.4rem]"
+                className="cursor-pointer font-mono text-[1.8rem] font-bold leading-[2.4rem] hover:text-a445ed"
                 onClick={() => {
                   closeNavBox();
                   setFont("mono");
